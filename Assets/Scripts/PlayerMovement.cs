b6986728eac1e9c1e85dd105cf2f714a040656e7
HrimiucDaniel <79639186+HrimiucDaniel@users.noreply.github.com>
2023-04-05 19:34:35 +0300

    attacking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{
+    walk,
+    attack,
+    interact
+}
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D rigidBody;
     private Vector3 change;
     private Animator animator;
+    public 
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
@@ -22,9 +30,22 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
-
+        if (Input.GetButtonDown("attack")  && currentState != PlayerState.attack) {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
         
+    }
+    private IEnumerator AttackCo() {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
     }
     
     void UpdateAnimationAndMove(){
