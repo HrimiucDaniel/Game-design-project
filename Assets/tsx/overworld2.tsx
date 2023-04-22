4171ac5d3a64bb9f8d2f3776b060863cefa6fab7
HrimiucDaniel <79639186+HrimiucDaniel@users.noreply.github.com>
2023-04-22 19:24:09 +0300

    updated + map

---
+++ Assets/tsx/overworld2.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="overworld2" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../Art/Overworld.png" width="640" height="576"/>
+ <wangsets>
+  <wangset name="path" type="mixed" tile="1201">
+   <wangcolor name="dirth" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="1160" wangid="2,2,2,1,2,2,2,2"/>
+   <wangtile tileid="1161" wangid="2,2,2,1,1,1,2,2"/>
+   <wangtile tileid="1162" wangid="2,2,2,2,2,1,2,2"/>
+   <wangtile tileid="1200" wangid="2,1,1,1,2,2,2,2"/>
+   <wangtile tileid="1201" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1202" wangid="2,2,2,2,2,1,1,1"/>
+   <wangtile tileid="1240" wangid="2,1,2,2,2,2,2,2"/>
+   <wangtile tileid="1241" wangid="1,1,2,2,2,2,2,1"/>
+   <wangtile tileid="1242" wangid="2,2,2,2,2,2,2,1"/>
+   <wangtile tileid="1280" wangid="1,1,2,2,2,1,1,1"/>
+   <wangtile tileid="1281" wangid="1,1,1,1,2,2,2,1"/>
+   <wangtile tileid="1282" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1320" wangid="2,2,2,1,1,1,1,1"/>
+   <wangtile tileid="1321" wangid="2,1,1,1,1,1,2,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
