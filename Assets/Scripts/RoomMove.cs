ee1109faebb17aecdf7f62cb2616bd3cc7c10348
HrimiucDaniel <79639186+HrimiucDaniel@users.noreply.github.com>
2023-06-01 17:45:05 +0300

    Bigger map + new scene

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -30,11 +30,7 @@ public class RoomMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-<<<<<<< Updated upstream
-        if (other.CompareTag("Player"))
-=======
         if (other.CompareTag("Player") && !other.isTrigger)
->>>>>>> Stashed changes
         {
             cam.minPosition += cameraChangemin;
             cam.maxPosition += cameraChangemax;
