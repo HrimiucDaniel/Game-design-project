88ca55123d4429a5d496a921a17214f0fbab4761
Alin Hîrtopanu <alinhirtopanu@gmail.com>
2023-06-07 21:38:41 +0300

    title screen

---
+++ Assets/Scripts/Game Stuff/TitleScreen.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Overworld");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
